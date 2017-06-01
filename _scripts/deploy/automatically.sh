851b73ff1d5662a3f84ec026e10f1083a05e0878
Nate <nate.wernimont@gmail.com>
2017-06-01 14:03:42 -0500

    Add a check to see if script executes

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,3 +1,5 @@
+echo automatically is executing
+
 echo $PASS | openconnect $USER@pyrite.cs.iastate.edu --passwd-on-stdin --user $USER --group SSLvpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
