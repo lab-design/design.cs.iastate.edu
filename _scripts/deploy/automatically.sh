eddafd62e7e7edd13136e119d404c5dddac4c6e3
Nate <nate.wernimont@gmail.com>
2017-06-01 14:35:08 -0500

    Make user public

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,5 +1,3 @@
-echo automatically is executing
-
 echo $PASS | openconnect $USER@pyrite.cs.iastate.edu --passwd-on-stdin --user $USER --usergroup SSLvpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
