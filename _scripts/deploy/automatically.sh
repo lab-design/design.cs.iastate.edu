068acb866d28fe3905ee2d044be61294993d8fe8
Nate <nate.wernimont@gmail.com>
2017-06-01 14:38:19 -0500

    Fix the vpn address

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,4 @@
-echo $PASS | openconnect $USER@pyrite.cs.iastate.edu --passwd-on-stdin --user $USER --usergroup SSLvpn
+echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --usergroup SSLvpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
       "$local_src" "$USER@$remote_host:$remote_dest"
