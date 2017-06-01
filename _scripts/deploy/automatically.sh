16aa0eb18a2b9aade1aa9ed8d98ef8c9b4d7e82c
Nate <nate.wernimont@gmail.com>
2017-06-01 14:24:57 -0500

    Fix openconnect command

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,6 +1,6 @@
 echo automatically is executing
 
-echo $PASS | openconnect $USER@pyrite.cs.iastate.edu --passwd-on-stdin --user $USER --group SSLvpn
+echo $PASS | openconnect $USER@pyrite.cs.iastate.edu --passwd-on-stdin --user $USER --usergroup SSLvpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
       "$local_src" "$USER@$remote_host:$remote_dest"
