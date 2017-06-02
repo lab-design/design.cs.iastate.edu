ec406e8a35bc0cd67947404691ae5e087ba48484
Nate <nate.wernimont@gmail.com>
2017-06-02 08:27:47 -0500

    Try a different group

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,4 @@
-echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --usergroup SSLvpn
+echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --usergroup ISU
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
       "$local_src" "$USER@$remote_host:$remote_dest"
