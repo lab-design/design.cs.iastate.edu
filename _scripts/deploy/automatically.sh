1a190fe528fba47ee176a699af66c7aec90e360a
Nate <nate.wernimont@gmail.com>
2017-06-02 10:00:28 -0500

    Fix a typo

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -3,5 +3,5 @@ echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER 
 echo Finished trying to connect to vpn
 
 rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
-      -e 'echo $PASS | sshpass -d 1 shh -q' \
+      -e 'echo $PASS | sshpass -d 1 ssh -q' \
       "$local_src" "$USER@$remote_host:$remote_dest"
