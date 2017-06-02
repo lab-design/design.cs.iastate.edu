34553c01dc5bfcfd52d6e23f0aaafab2d7a040b9
Nate <nate.wernimont@gmail.com>
2017-06-02 09:52:35 -0500

    Suppress pyrite server welcome message

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -2,7 +2,6 @@ echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER 
 
 echo Finished trying to connect to vpn
 
-rsync --version
-#rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
-#      -e 'echo $PASS | sshpass -d 1' \
-#      "$local_src" "$USER@$remote_host:$remote_dest"
+rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --quiet --delete --progress \
+      -e 'echo $PASS | sshpass -d 1' \
+      "$local_src" "$USER@$remote_host:$remote_dest"
