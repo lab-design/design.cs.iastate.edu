a208f314488286e910d7604aa354c1e043049f7a
Nate <nate.wernimont@gmail.com>
2017-06-02 08:31:26 -0500

    Try authgroup instead of usergroup

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,5 @@
-echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --usergroup ISU
+echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --authgroup ISU
+echo Finished trying to connect to vpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
       "$local_src" "$USER@$remote_host:$remote_dest"
