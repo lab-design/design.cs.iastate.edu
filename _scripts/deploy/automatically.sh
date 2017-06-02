41e1555b41d99629b7f136b2cc665f5a63d8d2e6
Nate <nate.wernimont@gmail.com>
2017-06-02 14:44:37 -0500

    Submit minor changes

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,6 +1,4 @@
-echo $PASS | sudo openconnect vpn.iastate.edu --background --passwd-on-stdin --user $USER --authgroup SSLvpn
-
-echo Finished trying to connect to vpn
+echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
 
 rsync --version
 
