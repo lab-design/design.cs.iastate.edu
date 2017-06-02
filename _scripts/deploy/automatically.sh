a1d57a04d69829127a48c15bc2cf901d895013e2
Nate <nate.wernimont@gmail.com>
2017-06-02 09:22:21 -0500

    Add verbose option to rsync for debugging

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,5 +1,7 @@
 echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
+
 echo Finished trying to connect to vpn
-rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
+
+rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
       "$local_src" "$USER@$remote_host:$remote_dest"
