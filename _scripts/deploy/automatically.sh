001b72222d0ff476e96c5ba1aabcfe375ea73cad
Nate <nate.wernimont@gmail.com>
2017-06-02 08:52:08 -0500

    Add a premade tun device

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,6 @@
-echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --authgroup SSLvpn
+ip tuntap add vpn0 mode tun
+
+echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --authgroup SSLvpn -s 'sudo -E /etc/vpnc/vpnc-script'
 echo Finished trying to connect to vpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
