82b19047dc4642ca2abf7c70692a848758a66f34
Nate <nate.wernimont@gmail.com>
2017-06-02 08:55:00 -0500

    Have openconnect use that pre made tun

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,6 +1,6 @@
 ip tuntap add vpn0 mode tun
 
-echo $PASS | openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --authgroup SSLvpn -s 'sudo -E /etc/vpnc/vpnc-script'
+echo $PASS | openconnect vpn.iastate.edu -i vpn0 --passwd-on-stdin --user $USER --authgroup SSLvpn -s 'sudo -E /etc/vpnc/vpnc-script'
 echo Finished trying to connect to vpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
