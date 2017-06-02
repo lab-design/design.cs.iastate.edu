551cce3be20b051bc7c72f78447372486e76b281
Nate <nate.wernimont@gmail.com>
2017-06-02 15:48:00 -0500

    Modify openconnect command

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,8 +1,8 @@
-echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
+echo $PASS | sudo openconnect vpn.iastate.edu -b --script=/etc/vpnc/vpnc-script --passwd-on-stdin --user $USER --authgroup SSLvpn
 
 rsync --version
 
 export SSHPASS="$PASS"
 
-rsync -W -vvv --recursive --delete --progress \
+rsync -W-vvv --recursive --delete --progress \
       "$local_src" "$USER@$remote_host:$remote_dest"
