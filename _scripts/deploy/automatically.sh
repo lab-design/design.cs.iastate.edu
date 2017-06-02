25abd05e7aacba1c7c7957085dee99560db584a1
Nate <nate.wernimont@gmail.com>
2017-06-02 09:03:56 -0500

    Allow openconnect to run in the background

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,4 @@
-echo $PASS | sudo openconnect vpn.iastate.edu --passwd-on-stdin --user $USER --authgroup SSLvpn
+echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
 echo Finished trying to connect to vpn
 rsync --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1' \
