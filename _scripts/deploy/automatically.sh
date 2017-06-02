a2b0f89878d95938a18cd8a7b791e6d9854c7260
Nate <nate.wernimont@gmail.com>
2017-06-02 10:15:52 -0500

    Update rsync to 3.1.1

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -2,6 +2,8 @@ echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER 
 
 echo Finished trying to connect to vpn
 
+rsync --version
+
 rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
       -e 'echo $PASS | sshpass -d 1 ssh -q' \
       "$local_src" "$USER@$remote_host:$remote_dest"
