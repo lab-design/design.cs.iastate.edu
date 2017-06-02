8be4014c67ac9a2981fa4f94c98aeee5f9f4f4fc
Nate <nate.wernimont@gmail.com>
2017-06-02 12:47:11 -0500

    Prevent openconnect from staying open

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,4 +1,4 @@
-echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
+echo $PASS | sudo openconnect vpn.iastate.edu --background --passwd-on-stdin --user $USER --authgroup SSLvpn
 
 echo Finished trying to connect to vpn
 
