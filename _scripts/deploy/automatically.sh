76e25690b1668c12017c15f7c7973d6a4475509b
Nate <nate.wernimont@gmail.com>
2017-06-02 09:38:06 -0500

    Find the version of rsync

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -2,6 +2,7 @@ echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER 
 
 echo Finished trying to connect to vpn
 
-rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
-      -e 'echo $PASS | sshpass -d 1' \
-      "$local_src" "$USER@$remote_host:$remote_dest"
+rsync --version
+#rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
+#      -e 'echo $PASS | sshpass -d 1' \
+#      "$local_src" "$USER@$remote_host:$remote_dest"
