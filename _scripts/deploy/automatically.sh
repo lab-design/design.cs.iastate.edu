64ad552df3ece2e8467ed6cb2a46262e7c6497b9
Nate <nate.wernimont@gmail.com>
2017-06-02 10:46:09 -0500

    Modify the rsync command

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,9 +1,9 @@
-echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
+echo $SSHPASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
 
 echo Finished trying to connect to vpn
 
 rsync --version
 
-echo $PASS | sshpass -d 1 rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
-      -e 'ssh -q' \
+rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
+      -e 'sshpass -e ssh -q' \
       "$local_src" "$USER@$remote_host:$remote_dest"
