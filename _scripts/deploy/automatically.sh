acf1abd2b1c456369a0a235e13ebfb93347146a5
Nate <nate.wernimont@gmail.com>
2017-06-02 10:55:33 -0500

    Use the Trusty OS

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,9 +1,9 @@
-echo $SSHPASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
+echo $PASS | sudo openconnect vpn.iastate.edu -b --passwd-on-stdin --user $USER --authgroup SSLvpn
 
 echo Finished trying to connect to vpn
 
 rsync --version
 
-rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
-      -e 'sshpass -e ssh -q' \
+echo $PASS | sshpass -d 1 rsync -vvv --perms --chmod=ug+rw,Dug+x,Fug-x,o-rwx --recursive --delete --progress \
+      -e 'ssh -q' \
       "$local_src" "$USER@$remote_host:$remote_dest"
