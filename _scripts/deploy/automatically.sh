dde56587da46a431f25102f658bc994ab8acc563
Nate <nate.wernimont@gmail.com>
2017-06-03 16:53:56 -0500

    Eliminate the factor causing openconnect to stall

--- _scripts/deploy/automatically.sh
+++ _scripts/deploy/automatically.sh
@@ -1,6 +1,7 @@
 echo Beginning openconnect
 
-echo $PASS | sudo openconnect vpn.iastate.edu --verbose --background --passwd-on-stdin --user $USER --authgroup SSLvpn
+#--script doesnt let --background work
+echo $PASS | sudo openconnect vpn.iastate.edu --background --passwd-on-stdin --user $USER --authgroup SSLvpn --script /_scripts/vpnc-script
 
 rsync --version
 
